bb071d5c10aa8de57e7a9d4095236c22843fa5ba
NehaTomar29 <neha@sns.edu.in>
2026-09-11 14:58:12 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Toaster } from 'sonner'
 
-// Components
-import Navbar from './components/Navbar.jsx'
-import Footer from './components/Footer.jsx'
-import ProtectedRoute from './components/ProtectedRoute.jsx'
+// UI Components
+import { NavigationMenu } from './components/ui/navigation-menu.jsx'
 
 // Pages
 import Home from './pages/Home.jsx'
@@ -23,7 +21,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <Router>
         <div className="min-h-screen flex flex-col bg-background text-foreground">
-          <Navbar />
+          <NavigationMenu />
           <main className="flex-grow">
             <Routes>
               <Route path="/" element={<Home />} />
@@ -31,17 +29,9 @@ function App() {
               <Route path="/institution/:id" element={<InstitutionDetail />} />
               <Route path="/review" element={<ReviewUs />} />
               <Route path="/login" element={<Login />} />
-              <Route 
-                path="/admin" 
-                element={
-                  <ProtectedRoute>
-                    <AdminDashboard />
-                  </ProtectedRoute>
-                } 
-              />
+              <Route path="/admin" element={<AdminDashboard />} />
             </Routes>
           </main>
-          <Footer />
           <Toaster position="top-right" />
         </div>
       </Router>
